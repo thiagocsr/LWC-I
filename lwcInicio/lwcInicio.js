01efbab0b64f23c177f9249f206ac9a0d360858f
thiagocsr <76794619+thiagocsr@users.noreply.github.com>
2023-04-26 09:32:34 -0300

    Update lwcInicio.js

--- lwcInicio/lwcInicio.js
+++ lwcInicio/lwcInicio.js
@@ -3,13 +3,13 @@ import { LightningElement } from 'lwc';
 export default class lwcInicio extends LightningElement {
     empresa = "Empresa tal";
     cnpj = "123123123123";
-    endereço = "Rua tal";
+    endereco = "Rua tal";
 
     produto = "Produto X";
-    descrição = "Aquele lá";
+    descricao = "Aquele lá";
     preço = "49.99";
 
-    promoção = "Só hoje";
+    promocao = "Só hoje";
     oferta1 = "2 por 89.99";
     oferta2 = "3 por 119.99"
     visivel = true;
